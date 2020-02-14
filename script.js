2122ec6f617109edfda34165d6575871c210279d
eagraf <graf.ethan@gmail.com>
2020-02-13 21:47:56 -0500

    Joke Order

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var myVar = setInterval(myTimer, 10000);
 
 function myTimer() {
     const jokes = [
-        "The only thing they hate more than each other is Donald Trump",
         "Why have one cranky Jewish guy when you could have two?",
+        "The only thing they hate more than each other is Donald Trump",
         "They're not even Democrats",
         "What are the chances they both die in office?",
         "A love story waiting to happen",
